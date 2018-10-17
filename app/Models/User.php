96bc921d0de847693810e2daf0d526033cd3a144
Mateusz Lencki <mateusz.lencki@itmdevlabs.com>
2018-10-17 20:43:26 +0200

    Laravel passport authorization integration

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,29 +1,19 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
 <?php
+
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/debug/check_auth', function () {
+        echo 'Auth works!';
+    });
+});
