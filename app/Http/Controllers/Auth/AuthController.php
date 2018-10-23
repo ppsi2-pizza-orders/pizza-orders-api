fc32685eee4e850673b337c4cf5bb32ca8c32c3a
Mateusz Lencki <mateusz.lencki@itmdevlabs.com>
2018-10-23 20:19:21 +0200

    Facebook auth method in AuthService

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Auth;
-use Socialite;
-use Redirect;
-use Exception;
 use Illuminate\Http\Request;
 
 use App\Services\AuthService;
-use App\Exceptions\ApiException;
 use App\Helpers\ApiResponse;
 
 class AuthController
@@ -27,22 +22,7 @@ class AuthController
     {
         $fb_access_token = $request->get('access_token');
 
-        if(!$fb_access_token) {
-            throw (new ApiException())->setMessage('Access token not provided');
-        }
-
-        try {
-            $fb_user = Socialite::driver('facebook')
-                ->userFromToken($fb_access_token);
-        } catch (Exception $exception) {
-            throw (new ApiException())
-                ->setErrorCode(400)
-                ->setMessage('Could not fetch Facebook account');
-        }
-
-        $user = $this->auth_service->findOrCreateUser($fb_user, 'facebook');
-
-        $access_token = $user->createToken('Api Token')->accessToken;
+        $access_token = $this->auth_service->facebookAuth($fb_access_token);
 
         return $this->response
             ->setMessage('Access token granted')
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,14 +2,39 @@
 
 namespace App\Services;
 
+use Auth;
+use Socialite;
+use Exception;
 use Hash;
 
 use App\Models\User;
 use Laravel\Socialite\Two\User as SocialUser;
 
+use App\Exceptions\ApiException;
+
 class AuthService
 {
-    public function findOrCreateUser(SocialUser $social_user, string $provider): User
+    public function facebookAuth(string $fb_access_token): string
+    {
+        if(!$fb_access_token) {
+            throw (new ApiException())->setMessage('Access token not provided');
+        }
+
+        try {
+            $fb_user = Socialite::driver('facebook')
+                ->userFromToken($fb_access_token);
+        } catch (Exception $exception) {
+            throw (new ApiException())
+                ->setErrorCode(400)
+                ->setMessage('Could not fetch Facebook account');
+        }
+
+        $user = $this->findOrCreateUser($fb_user, 'facebook');
+
+        return $user->createToken('Api Token')->accessToken;
+    }
+
+    private function findOrCreateUser(SocialUser $social_user, string $provider): User
     {
         $already_created_user = User::where('provider_id', $social_user->id)->first();
 
