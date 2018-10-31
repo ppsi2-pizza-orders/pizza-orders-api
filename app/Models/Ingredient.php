6dfb5461d93893247e07928431c6672e0961f3bb
FALOKOMP\Konrad <konrad.falinski.96@gmail.com>
2018-10-31 11:12:38 +0100

    Ingredient model&migration

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    protected $fillable = [
+        'name',
+        'price',
+        'image',
+        'available'
+    ];
+}
---
+++ database/migrations/2018_10_31_100211_create_ingredient_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIngredientTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredient', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->float ('price');
+            $table->string ('image')->nullable();
+            $table->boolean('available')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredient');
+    }
+}
