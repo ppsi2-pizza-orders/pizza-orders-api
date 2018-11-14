70f65f39543b617f028648adb37032b78bdb5032
Konrad Faliński <konrad.falinski.96@gmail.com>
2018-11-14 17:29:23 +0100

    Updated Restaurant Resource

--- app/Http/Resources/RestaurantResource.php
+++ app/Http/Resources/RestaurantResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\User;
 use App\Http\Resources\PizzaFullResource as FullPizza;
-
+use App\Http\Resources\ReviewResource as FullReview;
 
 class RestaurantResource extends JsonResource
 {
@@ -20,8 +20,9 @@ class RestaurantResource extends JsonResource
             'photo' => $this->photo,
             'description' => $this->description,
             'created_at' => $this->created_at,
-            'owner_id' => User::find($this->owner_id),
+            'owner_id' => User::find($this->owner_id, ['id', 'name']),
             'pizzas' => FullPizza::collection($this->pizzas),
+            'reviews' => FullReview::collection($this->reviews),
         ];
     }
 }
