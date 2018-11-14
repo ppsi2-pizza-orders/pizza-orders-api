cda42bf86c47f96a2fc18055ee710c990e4ff8f3
Konrad Faliński <konrad.falinski.96@gmail.com>
2018-11-14 17:31:07 +0100

    Updated migrations

--- database/migrations/2018_10_31_100211_create_ingredients_table.php
+++ database/migrations/2018_10_31_100211_create_ingredients_table.php
@@ -6,28 +6,18 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateIngredientsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->float ('price');
             $table->string ('image')->nullable();
-            $table->boolean('available')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('ingredients');
