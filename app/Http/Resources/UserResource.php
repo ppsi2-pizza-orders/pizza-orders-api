dfa43e787878901abd51efc5504ac5813476b5f8
Konrad Faliński <konrad.falinski.96@gmail.com>
2018-11-17 14:23:03 +0100

    Added user rank (Admin: true/false)

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -12,7 +12,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'roles' => []
+            'Admin' => $this->isAdmin,
         ];
     }
 }
