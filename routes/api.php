0657891160eb279ec2f8d2cc8dc638bf75717a4c
Mateusz Lencki <mateusz.lencki@gmail.com>
2018-12-09 23:45:26 +0100

    Remove comments from api.php

--- routes/api.php
+++ routes/api.php
@@ -5,30 +5,29 @@ Route::middleware('auth:api')->group(function () {
         echo 'Auth works!';
     });
 
-    Route::post('restaurant', 'MainRestaurant\RestaurantController@store'); // check doc
-    Route::patch('restaurant/{id}', 'MainRestaurant\RestaurantController@update'); // check doc
-    Route::delete('restaurant/{id}', 'MainRestaurant\RestaurantController@destroy'); // check doc
+    Route::post('restaurant', 'MainRestaurant\RestaurantController@store');
+    Route::patch('restaurant/{id}', 'MainRestaurant\RestaurantController@update');
+    Route::delete('restaurant/{id}', 'MainRestaurant\RestaurantController@destroy');
 
-    Route::post('restaurant/{id}/pizza', 'MainRestaurant\PizzaController@store'); //check doc
-    Route::patch('pizza/{id}', 'MainRestaurant\PizzaController@update'); // check doc
-    Route::delete('pizza/{id}', 'MainRestaurant\PizzaController@destroy'); // check doc
+    Route::post('restaurant/{id}/pizza', 'MainRestaurant\PizzaController@store');
+    Route::delete('pizza/{id}', 'MainRestaurant\PizzaController@destroy');
 
-    Route::post('ingredient', 'MainRestaurant\IngredientController@store'); // check doc doc
-    Route::patch('ingredient/{id}', 'MainRestaurant\IngredientController@update'); // check doc
-    Route::delete('ingredient/{id}', 'MainRestaurant\IngredientController@destroy'); // check doc
+    Route::post('ingredient', 'MainRestaurant\IngredientController@store');
+    Route::patch('ingredient/{id}', 'MainRestaurant\IngredientController@update');
+    Route::delete('ingredient/{id}', 'MainRestaurant\IngredientController@destroy');
 
-    Route::post('restaurant/{id}/review', 'MainRestaurant\ReviewController@store'); // check
-    Route::delete('review/{id}', 'MainRestaurant\ReviewController@destroy'); // check
+    Route::post('restaurant/{id}/review', 'MainRestaurant\ReviewController@store');
+    Route::delete('review/{id}', 'MainRestaurant\ReviewController@destroy');
 
-    Route::get('admin/restaurants', 'Admin\RestaurantController@index'); // check doc
-    Route::get('admin/ingredients', 'Admin\IngredientController@index'); // check doc
-    Route::get('admin/users', 'Admin\UserController@index'); // check doc
+    Route::get('admin/restaurants', 'Admin\RestaurantController@index');
+    Route::get('admin/ingredients', 'Admin\IngredientController@index');
+    Route::get('admin/users', 'Admin\UserController@index');
 });
 
-Route::post('auth/facebook', 'Auth\AuthController@facebookLogin'); // check doc
-Route::post('auth/register', 'Auth\AuthController@register'); // check doc
-Route::post('auth/login', 'Auth\AuthController@login'); // check doc
+Route::post('auth/facebook', 'Auth\AuthController@facebookLogin');
+Route::post('auth/register', 'Auth\AuthController@register');
+Route::post('auth/login', 'Auth\AuthController@login');
 
-Route::get('restaurants', 'MainRestaurant\RestaurantController@index'); // check doc
-Route::post('restaurants', 'MainRestaurant\RestaurantListController@search'); // check doc
-Route::get('restaurant/{id}', 'MainRestaurant\RestaurantController@show'); // check doc
+Route::get('restaurants', 'MainRestaurant\RestaurantController@index');
+Route::post('restaurants', 'MainRestaurant\RestaurantListController@search');
+Route::get('restaurant/{id}', 'MainRestaurant\RestaurantController@show');
