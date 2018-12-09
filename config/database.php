bdb2a29a66f6258044ca8b5de538ddfe7f4f1e02
Mateusz Lencki <mateusz.lencki@gmail.com>
2018-12-09 14:49:12 +0100

    Fix test environment

--- config/database.php
+++ config/database.php
@@ -56,12 +56,12 @@ return [
 
         'mysql_testing' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '33062'),
-            'database' => env('DB_DATABASE', 'pizza-orders-test'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'secret'),
-            'unix_socket' => env('TEST_DB_SOCKET', ''),
+            'host' => env('TEST_DB_HOST', '127.0.0.1'),
+            'port' => env('TEST_DB_PORT', '33062'),
+            'database' => env('TEST_DB_DATABASE', 'pizza-orders-test'),
+            'username' => env('TEST_DB_USERNAME', 'root'),
+            'password' => env('TEST_DB_PASSWORD', 'secret'),
+            'unix_socket' => env('TEST_TEST_DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
