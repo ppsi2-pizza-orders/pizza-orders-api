f9fc7997cd259a66d2bff280da1c0d019e75d0b3
Konrad Faliński <konrad.falinski.96@gmail.com>
2018-12-16 15:30:22 +0100

    Image link

--- app/Http/Resources/Restaurant/RestaurantResource.php
+++ app/Http/Resources/Restaurant/RestaurantResource.php
@@ -6,18 +6,20 @@ use App\Models\User;
 use App\Http\Resources\AbstractApiResource;
 use App\Http\Resources\PizzaFullResource as FullPizza;
 use App\Http\Resources\ReviewResource as FullReview;
+use Illuminate\Support\Facades\Config;
 
 class RestaurantResource extends AbstractApiResource
 {
     public function toArray(): array
     {
+
         return [
             'id' => $this->resource->id,
             'name' => $this->resource->name,
             'city' => $this->resource->city,
             'address' => $this->resource->address,
             'phone' => $this->resource->phone,
-            'photo' => $this->resource->photo,
+            'photo' => Config::get('constants.links.restaurant_photos').$this->resource->photo,
             'description' => $this->resource->description,
             'created_at' => $this->resource->created_at,
             'owner_id' => User::find($this->resource->owner_id, ['id', 'name']),
