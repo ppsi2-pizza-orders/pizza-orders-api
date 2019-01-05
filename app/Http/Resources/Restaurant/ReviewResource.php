07b1281c5ffdfcd63f9d4460b486855e574ef414
Mateusz Lencki <mateusz.lencki@gmail.com>
2019-01-05 18:06:30 +0100

    ReviewResource update

--- app/Http/Resources/Restaurant/ReviewResource.php
+++ app/Http/Resources/Restaurant/ReviewResource.php
@@ -14,9 +14,10 @@ class ReviewResource extends ApiResource
             'base_rating' => $this->resource->base_rating,
             'ingredients_rating' => $this->resource->ingredients_rating,
             'delivery_time_rating' => $this->resource->delivery_time_rating,
+            'average_rating' => floor($this->resource->average_rating * 2) / 2,
             'comment' => $this->resource->comment,
-            'created_at' => $this->resource->created_at,
-            'user_id' => User::find($this->resource->user_id, ['id', 'name'])
+            'created_at' => $this->resource->created_at->format('Y-m-d H:i:s'),
+            'user' => User::find($this->resource->user_id, ['id', 'name'])
         ];
     }
 }
