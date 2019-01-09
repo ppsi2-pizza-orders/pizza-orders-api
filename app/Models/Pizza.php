922beefbd50fa916e54cc2f9ae7e3fb58a6549a1
Konrad Faliński <konrad.falinski.96@gmail.com>
2019-01-09 22:45:27 +0100

    Updated relationships

--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -22,7 +22,7 @@ class Pizza extends Model
 
     public function ingredients()
     {
-        return $this->belongsToMany(Ingredient::class, 'pizza_ingredient');
+        return $this->belongsToMany(Ingredient::class);
     }
 }
 
