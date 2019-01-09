641f3a9e1faab90c5e05c3d74c05c54d9b83be91
Konrad Faliński <konrad.falinski.96@gmail.com>
2019-01-09 22:50:08 +0100

    Updated api.php

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('restaurant/{id}/pizza', 'MainRestaurant\PizzaController@store');
     Route::delete('pizza/{id}', 'MainRestaurant\PizzaController@destroy');
+    Route::post('pizza/{pizza_id}/attach', 'MainRestaurant\PizzaController@attach');
+    Route::post('pizza/{pizza_id}/detach', 'MainRestaurant\PizzaController@detach');
 
     Route::post('ingredient', 'MainRestaurant\IngredientController@store');
     Route::patch('ingredient/{id}', 'MainRestaurant\IngredientController@update');
@@ -19,6 +21,8 @@ Route::middleware('auth:api')->group(function () {
     Route::get('admin/ingredients', 'Admin\IngredientController@index');
     Route::get('admin/users', 'Admin\UserController@index');
     Route::get('admin/orders', 'Admin\OrderController@index');
+    Route::patch('admin/restaurant/{id}/publish', 'Admin\RestaurantController@publish');
+    Route::patch('admin/restaurant/{id}/hide', 'Admin\RestaurantController@hide');
 
     Route::post('order', 'Order\OrderController@placeOrder');
     Route::get('order/{token}', 'Order\OrderController@show');
@@ -27,6 +31,10 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('restaurant/{id}/grant', 'MainRestaurant\RestaurantOwnerController@grant');
     Route::delete('restaurant/{id}/revoke/{user_id}', 'MainRestaurant\RestaurantOwnerController@revoke');
+    Route::patch('restaurant/{id}/request', 'MainRestaurant\RestaurantOwnerController@request');
+    Route::patch('restaurant/{id}/cancel', 'MainRestaurant\RestaurantOwnerController@cancel');
+
+    Route::post('restaurant/{id}/manage', 'MainRestaurant\RestaurantManagerController@manage');
 });
 
 Route::post('auth/facebook', 'Auth\AuthController@facebookLogin');
@@ -37,4 +45,5 @@ Route::post('auth/refresh', 'Auth\AuthController@refreshToken');
 Route::get('restaurants', 'MainRestaurant\RestaurantController@index');
 Route::post('restaurants', 'MainRestaurant\RestaurantListController@search');
 Route::get('restaurant/{id}', 'MainRestaurant\RestaurantController@show');
+
 
