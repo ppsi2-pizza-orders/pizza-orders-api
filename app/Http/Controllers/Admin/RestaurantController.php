1db35d3736dcd601369e5d72857ef838b392a9c9
Konrad Faliński <konrad.falinski.96@gmail.com>
2019-01-09 22:49:01 +0100

    Added confirm restaurant method in admin controller

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
 use App\Http\Controllers\ApiResourceController;
 
@@ -12,6 +13,35 @@ class RestaurantController extends ApiResourceController
     {
         return $this->apiResource->response($request);
     }
+
+    public function publish($id) {
+        $restaurant = Restaurant::findOrFail($id);
+        if ($restaurant->visible == 0) {
+            throw $this->apiException
+                ->setStatusCode(400)
+                ->pushMessage('Can not confirm restaurant because status is set to private by owner');
+        }
+        if ($restaurant->confirmed == 1) {
+            throw $this->apiException
+                ->setStatusCode(400)
+                ->pushMessage('Restaurant already confirmed');
+        }
+        Restaurant::where('id', 'like', $id )->update(['confirmed' => true]);
+
+        return $this->apiResponse
+            ->pushMessage('Restaurant status changed to public')
+            ->response();
+    }
+
+    public function hide($id)
+    {
+        Restaurant::where('id', 'like', $id )->update(['confirmed' => false]);
+
+        return $this->apiResponse
+            ->pushMessage('Restaurant status changed to unconfirmed')
+            ->response();
+    }
+
 }
 
 
